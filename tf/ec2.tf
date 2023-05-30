c05660a52efa8a8fedc5a62b416ca45f6b043520
Ranjit Swain <58227542+ranjit4github@users.noreply.github.com>
2023-05-30 19:39:23 +0530

    Update ec2.tf

--- tf/ec2.tf
+++ tf/ec2.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_instance" "myinstance" {
   ami           = "ami-01a4f99c4ac11b03c"
   instance_type = "t2.micro"
-  key_name      = "pswain"
+  key_name      = "kswain"
   tags = {
     name = "TF-DemoServer"
   }
