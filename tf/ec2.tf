02a99bc4b226cd393fb507b059a972e474ad11c4
Ranjit Swain <58227542+ranjit4github@users.noreply.github.com>
2023-05-31 14:48:59 +0530

    Update ec2.tf

--- tf/ec2.tf
+++ tf/ec2.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIAVNW7YNJHNIJOMIFZ"
-  secret_key = "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"
 }
 resource "aws_instance" "myinstance" {
   ami           = "ami-01a4f99c4ac11b03c"
